280b7bfdb1bcbfd3019f1c471683459bdf135bbf
Ronnie Song <ronniesong0809@gmail.com>
2021-07-24 01:14:55 -0700

    twist thread num and sleep time

--- GetQuestions.py
+++ GetQuestions.py
@@ -44,7 +44,7 @@ class Spider:
             },
             "query": "query getQuestionDetail($titleSlug: String!) {\n  isCurrentUserAuthenticated\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    questionTitle\n    questionTitleSlug\n    content\n    translatedContent\n    difficulty\n    stats\n    allowDiscuss\n    contributors {\n      username\n      profileUrl\n      __typename\n    }\n    similarQuestions\n    mysqlSchemas\n    randomQuestionUrl\n    sessionId\n    categoryTitle\n    submitUrl\n    interpretUrl\n    codeDefinition\n    sampleTestCase\n    enableTestMode\n    metaData\n    enableRunCode\n    enableSubmit\n    judgerAvailable\n    infoVerified\n    envInfo\n    urlManager\n    article\n    questionDetailUrl\n    libraryUrl\n    adminUrl\n    companyTags {\n      name\n      slug\n    }\n    companyTagStats\n    topicTags {\n      name\n      slug\n    }\n    __typename\n  }\n  interviewed {\n    interviewedUrl\n    companies {\n      id\n      name\n      slug\n      __typename\n    }\n    timeOptions {\n      id\n      name\n      __typename\n    }\n    stageOptions {\n      id\n      name\n      __typename\n    }\n    __typename\n  }\n  subscribeUrl\n  isPremium\n  loginUrl\n}\n"
         }
-        sleep(randint(0,1))
+        sleep(randint(0, 5))
         r = requests.post(url=url, headers=headers, data=json.dumps(body))
 
         if r.status_code == 200:
@@ -53,7 +53,7 @@ class Spider:
             return response["data"]["question"]["topicTags"], json.loads(response["data"]["question"]["similarQuestions"]), response["data"]["question"]["companyTags"], json.loads(response["data"]["question"]["companyTagStats"]), response["data"]["question"]["envInfo"]
         else:
             self.timeoutCounter = self.timeoutCounter + 1
-            sleep(randint(10,15))
+            sleep(randint(1, 10))
             print(f'[{r.status_code}] {id:>5}')
             return self.processResponse(slug, id)
 
